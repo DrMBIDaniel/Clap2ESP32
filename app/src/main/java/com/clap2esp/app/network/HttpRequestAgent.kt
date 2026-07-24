d40f00af0169458d381f567460f282d520fd4995
DrMBIDaniel <danyalmurat96@gmail.com>
2026-07-24 17:28:45 +0500

    Create HttpRequestAgent.kt

---
+++ app/src/main/java/com/clap2esp/app/network/HttpRequestAgent.kt
@@ -0,0 +1,16 @@
+package your.package.network
+
+class HttpRequestAgent : RequestAgent {
+
+    override fun sendToggle() {
+        // Пока пусто
+    }
+
+    override fun sendOn() {
+        // Пока пусто
+    }
+
+    override fun sendOff() {
+        // Пока пусто
+    }
+}
