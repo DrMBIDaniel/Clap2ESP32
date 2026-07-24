c614d16f8d32bb4b3f6428232d90b95360be0078
DrMBIDaniel <danyalmurat96@gmail.com>
2026-07-25 04:11:08 +0500

    Update HttpRequestAgent.kt

--- app/src/main/java/com/clap2esp/app/network/HttpRequestAgent.kt
+++ app/src/main/java/com/clap2esp/app/network/HttpRequestAgent.kt
@@ -1,16 +1,31 @@
-package com.clap2esp.app
+package com.clap2esp.app.network
 
-class HttpRequestAgent : RequestAgent {
+import android.content.Context
+import com.clap2esp.app.SettingsRepository
 
-    override fun sendToggle() {
-        // Пока пусто
+class HttpRequestAgent(
+    context: Context
+) : RequestAgent {
+
+    private val repository = SettingsRepository(context)
+
+    override fun sendToggle(): Boolean {
+
+        val settings = repository.load()
+
+        println(settings.espAddress)
+        println(settings.togglePath)
+
+        return true
     }
 
-    override fun sendOn() {
-        // Пока пусто
+    override fun sendOn(): Boolean {
+
+        return true
     }
 
-    override fun sendOff() {
-        // Пока пусто
+    override fun sendOff(): Boolean {
+
+        return true
     }
 }
