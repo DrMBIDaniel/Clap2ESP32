4c79cee9730e7ab4072db8df789663a0192f5a30
DrMBIDaniel <danyalmurat96@gmail.com>
2026-07-24 17:53:18 +0500

    Create SettingsActivity.kt

---
+++ app/src/main/java/com/clap2esp/app/settings/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.clap2esp.app.settings
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.clap2esp.app.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
