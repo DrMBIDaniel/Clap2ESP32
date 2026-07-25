aedc5768727f767c64473a0b548c5d7e9d8dd84a
DrMBIDaniel <danyalmurat96@gmail.com>
2026-07-25 09:46:17 +0500

    Update CommandProcessor.kt

--- app/src/main/java/com/clap2esp/app/command/CommandProcessor.kt
+++ app/src/main/java/com/clap2esp/app/command/CommandProcessor.kt
@@ -1,53 +1,38 @@
 package com.clap2esp.app.command
 
+import com.clap2esp.app.Logger
+import com.clap2esp.app.network.RequestAgent
+
 class CommandProcessor(
     private val requestAgent: RequestAgent
 ) {
 
     fun onSingleClap() {
-
         Logger.log("Single clap")
-
     }
 
     fun onDoubleClap() {
-
         Logger.log("Double clap")
-
         requestAgent.sendToggle()
-
     }
 
     fun onSequenceDoubleSingle() {
-
         Logger.log("Double → Single")
-
         requestAgent.sendOff()
-
     }
 
     fun onSequenceSingleDouble() {
-
         Logger.log("Single → Double")
-
         requestAgent.sendOff()
-
     }
 
     fun onLightOn() {
-
         Logger.log("Light ON")
-
         requestAgent.sendOn()
-
     }
 
     fun onLightOff() {
-
         Logger.log("Light OFF")
-
         requestAgent.sendOff()
-
     }
-
 }
