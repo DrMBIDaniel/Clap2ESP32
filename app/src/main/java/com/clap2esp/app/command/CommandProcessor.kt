45134ff4bf5bbbf27d6584e9bd06cc8076ff431c
DrMBIDaniel <danyalmurat96@gmail.com>
2026-07-25 11:17:50 +0500

    Update CommandProcessor.kt

--- app/src/main/java/com/clap2esp/app/command/CommandProcessor.kt
+++ app/src/main/java/com/clap2esp/app/command/CommandProcessor.kt
@@ -20,8 +20,11 @@ private var lastClapTime = 0L
 private val sequenceTimeout = 1200L
 
     fun onSingleClap() {
-        Logger.log("Single clap")
-    }
+
+    Logger.log("Single clap")
+
+    processSequence(ClapType.SINGLE)
+}
 
     fun onDoubleClap() {
 
@@ -45,6 +48,7 @@ private val sequenceTimeout = 1200L
     } else {
 
         requestAgent.sendOff()
+        processSequence(ClapType.DOUBLE)
 
     }
 
@@ -69,4 +73,48 @@ private val sequenceTimeout = 1200L
         Logger.log("Light OFF")
         requestAgent.sendOff()
     }
+
+    private fun processSequence(type: ClapType) {
+
+    val now = System.currentTimeMillis()
+
+    if (lastClap != null &&
+        now - lastClapTime < sequenceTimeout
+    ) {
+
+        val settings = repository.load()
+
+        if (
+            lastClap == ClapType.DOUBLE &&
+            type == ClapType.SINGLE
+        ) {
+
+            if (settings.sequence == 0) {
+
+                Logger.log("Sequence: DOUBLE -> SINGLE")
+
+                onSequenceDoubleSingle()
+            }
+
+        }
+
+        if (
+            lastClap == ClapType.SINGLE &&
+            type == ClapType.DOUBLE
+        ) {
+
+            if (settings.sequence == 1) {
+
+                Logger.log("Sequence: SINGLE -> DOUBLE")
+
+                onSequenceSingleDouble()
+            }
+
+        }
+
+    }
+
+    lastClap = type
+    lastClapTime = now
+}
 }
