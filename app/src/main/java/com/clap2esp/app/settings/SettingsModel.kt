a7109aca44d0ed347dfabc7ba3c09b77fe32e46b
DrMBIDaniel <danyalmurat96@gmail.com>
2026-07-27 12:12:27 +0500

    Update SettingsModel.kt

--- app/src/main/java/com/clap2esp/app/settings/SettingsModel.kt
+++ app/src/main/java/com/clap2esp/app/settings/SettingsModel.kt
@@ -1,13 +1,17 @@
 package com.clap2esp.app.settings
 
 data class SettingsModel(
-    val espAddress:String="",
-    val togglePath:String="/toggle",
-    val mode:Int=0,
-    val sequence:Int=0,
-    val manualTimeout:Boolean=false,
-    val timeout:Int=550,
-    val vibration:Boolean=true,
-    val networkLog:Boolean=false,
-    val connectionIndicator:Boolean=true
+    
+    val espAddress: String = "",
+    val togglePath: String = "/toggle",
+    val onPath: String = "/on",
+    val offPath: String = "/off",
+    val mode: Int = 0,
+    val sequence: Int = 0,
+    val manualTimeout: Boolean = false,
+    val timeout: Int = 550,
+    val vibration: Boolean = true,
+    val networkLog: Boolean = false,
+    val connectionIndicator: Boolean = true
+
 )
