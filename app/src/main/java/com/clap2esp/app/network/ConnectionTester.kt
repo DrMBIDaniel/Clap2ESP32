23b9f0714b694c3d5b425a89a32d226ba4bcbb29
DrMBIDaniel <danyalmurat96@gmail.com>
2026-07-27 17:18:01 +0500

    Update ConnectionTester.kt

--- app/src/main/java/com/clap2esp/app/network/ConnectionTester.kt
+++ app/src/main/java/com/clap2esp/app/network/ConnectionTester.kt
@@ -1,17 +1,51 @@
 package com.clap2esp.app.network
 
 import android.content.Context
+import com.clap2esp.app.LogCategory
+import com.clap2esp.app.LogType
+import com.clap2esp.app.Logger
+import com.clap2esp.app.settings.SettingsRepository
 
 class ConnectionTester(
     context: Context
 ) {
 
-    private val agent = HttpRequestAgent(context)
+    private val requestAgent = HttpRequestAgent(context)
 
-    fun test(): Boolean {
+    fun testToggle(): Boolean {
 
-        return agent.sendToggle()
+        Logger.log(
+            "Testing Toggle command",
+            LogType.INFO,
+            LogCategory.NET
+        )
 
+        return requestAgent.sendToggle()
     }
 
+    fun testPrimary(): Boolean {
+
+        Logger.log(
+            "Testing Primary command",
+            LogType.INFO,
+            LogCategory.NET
+        )
+
+        return requestAgent.sendOn()
+    }
+
+    fun testSecondary(): Boolean {
+
+        Logger.log(
+            "Testing Secondary command",
+            LogType.INFO,
+            LogCategory.NET
+        )
+
+        return requestAgent.sendOff()
+    }
+
+    fun test(): Boolean {
+        return testToggle()
+    }
 }
